cf4424331f904c257bad0b9a1c8442d5314c625b
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2022-01-06 01:12:12 -0600

    Implement initial header (WIP)
    Adds initial header to match UI design. No logo yet, and slogan is undecided.

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 
+import './styles/HomePage.css';
+import './styles/theme.css';
+
 interface IHomePageState {
 }
 
@@ -15,10 +18,24 @@ export class HomePage extends React.Component<IHomePageProps, IHomePageState> {
         this.state = {
         }
     }
-    
+
     render() {
         return (
             <div>
+                <header>
+                    <div className="banner">
+                        <div className="logo">Dino Travel Logo</div>
+                        <div className="slogan">
+                            <h2 className="slogan">Travel More (slogan)</h2>
+                        </div>
+                    </div>
+
+                    <nav>
+                        <button>support</button>
+                        <button>about us</button>
+                        <button>trips</button>
+                    </nav>
+                </header>
                 <h1>Test</h1>
             </div>
         )
