88e0d65e319efb9d71362c1a603a969eac23e2b0
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2022-01-08 23:50:13 -0600

    Update base url for flights endpoint; url using https + certificate

--- src/api/flights.ts
+++ src/api/flights.ts
@@ -1,7 +1,7 @@
-const serverIP = '35.171.66.24';
+const baseURL = 'purpledinoapi.link';
 const port = '8080';
 const flightsAPI = '/api/flights';
-const flightEndpointURL = `https://${serverIP}:${port}${flightsAPI}`;
+const flightEndpointURL = `https://${baseURL}:${port}${flightsAPI}`;
 
 export interface IFlightData {
     arrival_airport: string; // 3 character abbreviation of the airport name
