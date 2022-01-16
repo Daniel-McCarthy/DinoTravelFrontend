26919ece635ea79e569b19db165f9c5886cc78a3
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2022-01-15 23:16:07 -0600

    Add helper function to convert FlightType enum into JSON backend values
    Adds helper function so that we can take the Flight Type enum and use it to convert it to the backend value used in JSON. This allows us to use the enum consistently and use it for the frontend labels, and quickly convert using the same enum when we want to create a backend API call.

--- src/enums/FlightType.ts
+++ src/enums/FlightType.ts
@@ -2,3 +2,12 @@ export enum FlightType {
     OneWay = 'One Way',
     RoundTrip = 'Round Trip'
 }
+
+export enum FlightTypeJsonLabel {
+    'One Way' = 'ONE_WAY',
+    'Round Trip' = 'ROUND_TRIP',
+}
+
+export const flightTypeAsJsonLabel = (flightTypeValue: FlightType): FlightTypeJsonLabel => {
+    return FlightTypeJsonLabel[flightTypeValue];
+};
