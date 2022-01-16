17be151f628224e4c26ff95598c95f79d4623dc5
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2022-01-15 23:15:41 -0600

    Add helper function to convert FlightClass enum into JSON backend values
    Adds helper function so that we can take the Flight Class enum and use it to convert it to the backend value used in JSON. This allows us to use the enum consistently and use it for the frontend labels, and quickly convert using the same enum when we want to create a backend API call.

--- src/enums/FlightClass.ts
+++ src/enums/FlightClass.ts
@@ -3,3 +3,14 @@ export enum FlightClass {
     EconomyClass = 'Economy Class',
     BusinessClass = 'Business Class'
 }
+
+export enum FlightClassJsonLabel {
+    'First Class' = 'FIRST_CLASS',
+    'Economy Class' = 'ECONOMY',
+    'Business Class' = 'BUSINESS'
+    // Premium Economy found in backend not yet accounted for due to not being in the requirements.
+}
+
+export const flightClassAsJsonLabel = (classValue: FlightClass): FlightClassJsonLabel => {
+    return FlightClassJsonLabel[classValue];
+};
