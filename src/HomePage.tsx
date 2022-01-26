33a84371fd8256de71f9ff19fe9f56e94f0bfd91
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2022-01-26 01:17:57 -0600

    Update Date Pickers to work on Safari
    Safari doesn't support the date input so must use a fallback of using placeholder to input the date format.

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -187,13 +187,15 @@ export class HomePage extends React.Component<IHomePageProps, IHomePageState> {
                         </div>
                         <div className="dateInputContainer">
                             <h3>Departing</h3>
-                            <input className="datePicker" type="date" onChange={this.onArrivalFlightDateSelected}></input>
+                            {/* Placeholder is used as a fallback on browsers that don't support the datepicker, e.g. Safari and IE */}
+                            <input className="datePicker" type="date" onChange={this.onArrivalFlightDateSelected} placeholder="yyyy-mm-dd"></input>
                         </div>
                         
                         {isRoundTripSelected 
                             ? <div className="dateInputContainer">
                                 <h3>Returning</h3>
-                                <input className="datePicker" onChange={this.onReturnFlightDateSelected} type="date"></input>
+                                {/* Placeholder is used as a fallback on browsers that don't support the datepicker, e.g. Safari and IE */}
+                                <input className="datePicker" onChange={this.onReturnFlightDateSelected} type="date" placeholder="yyyy-mm-dd"></input>
                             </div>
                             : null
                         }
