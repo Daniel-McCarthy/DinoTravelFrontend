bfe64cc9f3c273e2f4340e330b3ff83705d1a812
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2022-01-30 18:07:11 -0600

    AirportSelector: Handle focus gain and loss & child element focus
    Adds functions for on focus to inform the react state when our input component has gained focus and when it has lost focus. This is going to be used for determining when to and not to display the results list dropdown when performing a search.
    
    However, input focus is not sufficient because onBlur will cause the dropdown to disappear even when clicking a list item from the dropdown, even though it's part of the component. So we need to check if the element being switched to is a child of the AirportSelector and not dismiss the dropdown in that scenario. (If we don't, the dropdown will close when clicking a list item, but the list item doesn't receive the click)

--- src/components/AirportSelector.tsx
+++ src/components/AirportSelector.tsx
@@ -7,6 +7,8 @@ import { getLocationsForQuery, ILocationData } from "../api/locations";
 
 interface IAirportSelectorState {
     locationResults: ILocationData[];
+    isInputFocused: boolean;
+    hasFirstQueryBeenMade: boolean; // So that we don't show the user that no results were found upon initially typing
 
     selectedLocation: ILocationData | null;
 }
@@ -16,13 +18,51 @@ interface IAirportSelectorProps {
 
 export class AirportSelector extends React.Component<IAirportSelectorProps, IAirportSelectorState> {
 
+    selectorContainerRef: React.RefObject<HTMLDivElement>;
+
     public constructor(props: IAirportSelectorProps) {
         super(props)
 
+        this.selectorContainerRef = React.createRef();
+
         this.state = {
             locationResults: [],
+            isInputFocused: false,
+            hasFirstQueryBeenMade: false,
             selectedLocation: null
         }
+    }
+
+    onInputFocus = () => {
+        // When the input element is focused, we set that in state so we know
+        // to display the results list (if a sufficient query is entered).
+        this.setState({
+            isInputFocused: true
+        });
+    };
+
+    onInputBlur = () => {
+        // Check if focus shifted to a child element, if so, keep the selection list open.
+        const currentlyFocusedElement = document.activeElement;
+        const selectorContainerElement: HTMLDivElement | null = this.selectorContainerRef.current;
+        if (!!selectorContainerElement && !!currentlyFocusedElement && this.isElementParentOf(selectorContainerElement, currentlyFocusedElement)) {
+            return;
+        }
+
+        // The focus has been lost on the Selector input and the new focus element is not a child of the AirportSelector.
+        // Closing the list since focus is now on an unrelated component or element.
+        this.setState({
+            isInputFocused: false
+        });
+    }
+
+    isElementParentOf = (parentElement: Element, childElement: Element) => {
+        // Uses the Node class to check if a HTMLElement is a descendant of another.
+        // The Node compareDocumentPosition function returns a bitmask containing bits
+        // that encode information about their dom structure. We just want to see if 
+        // the parent contains the child, so we check if the Node.DOCUMENT_POSITION_CONTAINS (8)
+        // bit is set in the mask or not. If it is, the the child element DOES descend from the parent.
+        return (parentElement.compareDocumentPosition(childElement) & Node.DOCUMENT_POSITION_CONTAINS) !== 0;
     }
 
     render() {
