b59de712eabf312914396f00a4ee1c016f137da4
(Clark61) <clark6147@gmail.com>
2022-02-02 02:05:52 -0600

    Started trips page

---
+++ src/TripsPage.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+interface ITripsPageState {
+
+}
+
+interface ITripsPageProps {
+
+}
+
+export class TripsPage extends React.Component<ITripsPageProps, ITripsPageState> {
+
+    public constructor(props: ITripsPageProps) {
+        super(props)
+        this.state = {}
+    }
+
+
+    render() {
+        return (
+            <>
+                // TODO header
+                
+                // TODO Main
+            </>
+        )
+    }
+}
