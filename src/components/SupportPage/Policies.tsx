f48dc3552ca276e68deb145f7230552dd160b420
(Clark61) <clark6147@gmail.com>
2022-02-02 02:06:03 -0600

    Added policies component

---
+++ src/components/SupportPage/Policies.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default function Policies() {
+    return (
+        <>
+            <div id="policies">
+                <div className="Policy1">
+                    <h2>Policy Title 1</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                        aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est 
+                        laborum.</p>
+                </div>
+
+                <div className="Policy2">
+                    <h2>Policy Title 2</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                        aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est 
+                        laborum.</p>
+                </div>
+
+                <div className="Policy3">
+                    <h2>Policy Title 3</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                        aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est 
+                        laborum.</p>
+                </div>
+            </div>  
+        </>
+    );
+}
