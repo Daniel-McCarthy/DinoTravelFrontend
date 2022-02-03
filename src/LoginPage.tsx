349ed2714587273eb025366416a473d7efec7f8b
David Gawel <DGawel77@gmail.com>
2022-02-03 01:15:06 -0600

    Added very basic google OAUTH support

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,5 +1,18 @@
 import * as React from "react";
+import GoogleLogin from "react-google-login";
 
+const responseGoogle = (response: any) => {
+    console.log('FAILURE');
+    console.log(response);
+}
+
+function onSignIn(googleUser : any) {
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
 interface ILoginPageState {
 }
@@ -19,8 +32,14 @@ export class LoginPage extends React.Component<ILoginPageProps, ILoginPageState>
 
     render() {
         return (
-            <div>
-            </div>
+            <GoogleLogin
+                clientId="1042234633479-gpprc2adcpltfjnaij7gib55ko91441n.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={onSignIn}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+
         )
     }
 }
