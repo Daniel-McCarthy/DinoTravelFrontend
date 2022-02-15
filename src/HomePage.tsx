7ebb95bd3660d3673dad0c7c5eff71e6de48f67b
Dan McCarthy <dan.mccarthy.software@gmail.com>
2022-02-14 18:52:44 -0600

    Add handling of multiple flights to search on Home Page (For all FlightType searches)
    Adds state and helper functions for tracking which flights need to be searched and tracking search status. This will be used for selecting several flights in a row and seeing when we are done and ready to submit.
    
    This will allow us to treat One Way, Round Trip, and Multi-City the same way and use the same logic for flight selection.

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -27,6 +27,31 @@ import { ILocationData } from "./api/locations";
 import { getFlightOffersWithFilters, IFlightOfferArguments, IFlightOfferData } from "./api/flightOffers";
 const bannerImages = [ bannerImage1, bannerImage2, bannerImage3, bannerImage4, bannerImage5, bannerImage6, bannerImage7, bannerImage8 ];
 
+interface IFlight {
+    originAirportIataCode: string;
+    destinationAirportIataCode: string;
+    flightDate: moment.Moment;
+}
+
+interface IFlightSearch {
+    flight: IFlight;
+    numAdultTravelers: number;
+    numChildTravelers: number;
+    hasBeenSearched: boolean;
+    selectedFlightOffer?: IFlightOfferData;
+}
+
+enum SearchStatus {
+    SettingFilters,
+    Searching,
+    Finished
+}
+
+export interface ISearchProgress {
+    searchStatus: SearchStatus;
+    flightIndexBeingSearched : number;
+    flightsTotalToSearch: number;
+}
 
 
 interface IHomePageState {
@@ -48,11 +73,14 @@ interface IHomePageState {
     returnAirport: ILocationData | null;
     returnFlightDate: moment.Moment;
 
+    searchStatusMode: SearchStatus;
     // A copy of any selected flights to search for in multi-city mode.
     // These flights are passed up from the MultiCityFlightSelection component.
     multiCityFlightOfferSelections: Flight[];
 
     bannerImages: string[];
+    currentSearches: IFlightSearch[];
+    searchProgress: ISearchProgress;
 }
 
 interface IHomePageProps {
@@ -86,27 +114,161 @@ export class HomePage extends React.Component<IHomePageProps, IHomePageState> {
             returnAirport: null,
             returnFlightDate: moment(),
 
-            multiCityFlightSelections: [],
-
-            bannerImages
+            multiCityFlightOfferSelections: [],
+            bannerImages,
+            currentSearches: [],
+            searchStatusMode: SearchStatus.SettingFilters,
+            searchProgress: {
+                searchStatus: SearchStatus.SettingFilters,
+                flightIndexBeingSearched: 0,
+                flightsTotalToSearch: 0
+            }
         }
     }
 
-    getFlightOfferAPIData = async () => {
-        if (!this.state.departureAirport || !this.state.returnAirport) {
-            // handle error
-            return;
+
+
+
+    getCurrentFlightToSelect = (): IFlightSearch | null => {
+        for (const flightSearch of this.state.currentSearches) {
+            if (!flightSearch.hasBeenSearched)
+                return flightSearch
         }
+        return null;
+    }
+
+    resetFlightSearches = () => {
+        const searchesWithClearedOffers = this.state.currentSearches.map(flightSearch => {
+            flightSearch.hasBeenSearched = false;
+            flightSearch.selectedFlightOffer = undefined;
+            return flightSearch;
+        });
+        this.setState({
+            currentSearches: searchesWithClearedOffers
+        });
+    }
+
+    selectOfferForCurrentFlightSearch = (selectedOffer: IFlightOfferData) => {
+        const updatedSearches = this.state.currentSearches.map(flightSearch => {
+            // If the flight has already been searched (previous search), simply return it to the array unmodified.
+            if (flightSearch.hasBeenSearched)
+                return flightSearch;
+
+            // If the flight hasn't been searched, it is the current active search.
+            // Update this flight with the new offer selection.
+            flightSearch.hasBeenSearched = true;
+            flightSearch.selectedFlightOffer = selectedOffer;
+            return flightSearch;
+        });
+        this.setState({
+            currentSearches: updatedSearches
+        });
+    }
+
+    assembleFlightSearchList = (): IFlightSearch[] => {
+
+        const flightType = this.state.flightType;
+        if (flightType === FlightType.MultiCity) {
+            if (this.state.multiCityFlightOfferSelections.length <= 0) {
+                // Add proper handling
+                return[];
+            }
+
+            const flightsToSearch: IFlightSearch[] = [];
+            this.state.multiCityFlightOfferSelections.forEach(flightToSearch => {
+                const originLocation = flightToSearch.leavingFrom;
+                const destinationLocation = flightToSearch.goingTo;
+                if (originLocation == null || destinationLocation == null) {
+                    console.error(`Error assembling flightList. Found following selected flight to search with missing destination or origin data:\n${JSON.stringify(flightToSearch)}`);
+                    return;
+                }
+                const asFlightInterface: IFlight = {
+                    originAirportIataCode: originLocation.iataCode,
+                    destinationAirportIataCode: destinationLocation?.iataCode,
+                    flightDate: flightToSearch.depatureDate
+                };
+                const asFlightSearchInterface: IFlightSearch = {
+                    flight: asFlightInterface,
+                    numAdultTravelers: this.state.numAdultTravelers,
+                    numChildTravelers: this.state.numChildTravelers,
+                    hasBeenSearched: false
+                };
+                flightsToSearch.push(asFlightSearchInterface);
+            });
+            // Set search list for all flights configured via multicity selection
+            // TODO: Bubble up error to user if searching incomplete selection in multi-city instead of just ignoring it and printing to console
+            this.setState({
+                currentSearches: flightsToSearch
+            });
+            return flightsToSearch;
+
+        } else {
+            if (!this.state.returnAirport || !this.state.departureAirport) {
+                // Add proper handling
+                return [];
+            }
+
+            const flightsToSearch: IFlightSearch[] = [];
+            const firstFlight: IFlight = {
+                originAirportIataCode: this.state.departureAirport?.iataCode,
+                destinationAirportIataCode: this.state.returnAirport?.iataCode,
+                flightDate: this.state.departureFlightDate
+            };
+            const firstFlightSearch: IFlightSearch = {
+                flight: firstFlight,
+                numAdultTravelers: this.state.numAdultTravelers,
+                numChildTravelers: this.state.numChildTravelers,
+                hasBeenSearched: false
+            };
+
+            flightsToSearch.push(firstFlightSearch);
+            if (this.state.flightType === FlightType.RoundTrip) {
+                // Add return trip
+                const returnOrigin = this.state.returnAirport;
+                const returnDestination = this.state.departureAirport;
+
+                const secondFlightSearch: IFlightSearch = {
+                    flight: {
+                        originAirportIataCode: returnOrigin.iataCode,
+                        destinationAirportIataCode: returnDestination.iataCode,
+                        flightDate: this.state.returnFlightDate
+                    },
+                    numAdultTravelers: this.state.numAdultTravelers,
+                    numChildTravelers: this.state.numChildTravelers,
+                    hasBeenSearched: false
+                };
+                flightsToSearch.push(secondFlightSearch);
+            }
+
+            // Set outbound (and if RoundTrip, inbound too) flight to currentSearches to go through.
+            this.setState({
+                currentSearches: flightsToSearch
+            });
+            return flightsToSearch;
+        }
+    }
+
+
+
+    getFlightOfferAPIData = async () => {
 
         try {
+            const currentFlightToSearch = this.getCurrentFlightToSelect();
+            if (currentFlightToSearch === null) {
+                // Handle error
+                return;
+            }
+
             const flightOfferArguments: IFlightOfferArguments = {
-                departureAirportIataCode: this.state.departureAirport?.iataCode,
+                departureAirportIataCode: currentFlightToSearch?.flight.originAirportIataCode,
                 departureDate: this.state.departureFlightDate,
-                arrivalAirportIataCode: this.state.returnAirport?.iataCode,
-                returnDate: this.state.returnFlightDate,
+                arrivalAirportIataCode: currentFlightToSearch?.flight.destinationAirportIataCode,
                 numberOfAdults: this.state.numAdultTravelers,
                 numberOfChildren: this.state.numChildTravelers,
             };
+
+            if (this.state.flightType === FlightType.RoundTrip)
+                flightOfferArguments.returnDate = this.state.returnFlightDate;
             const flightOffers: Array<IFlightOfferData> | Error = await getFlightOffersWithFilters(flightOfferArguments);
             console.log(flightOffers);
 
@@ -168,6 +330,7 @@ export class HomePage extends React.Component<IHomePageProps, IHomePageState> {
         const roundTripButtonClass = isRoundTripSelected ? 'selected' : '';
         const oneWayButtonClass = isOneWaySelected ? 'selected' : '';
         const multiCityButtonClass = isMultiCitySelected ? 'selected' :  '';
+
         return (
             <div>
                 <header>
@@ -262,7 +425,7 @@ export class HomePage extends React.Component<IHomePageProps, IHomePageState> {
                         </div>
                     }
 
-                    <FlightList flightOfferData={this.state.flightOfferData} onFlightSelectionUpdate={this.selectedFlightOfferUpdated} hide={!this.state.showingFlightList}></FlightList>
+                    <FlightList flightSearchStatus={this.state.searchProgress} flightSelectionType={this.state.flightType} flightOfferData={this.state.flightOfferData} onFlightSelectionUpdate={this.selectedFlightOfferUpdated} hide={!this.state.showingFlightList}></FlightList>
 
                     <button className="nontoggle" id="searchButton" onClick={this.onSearchClicked}>Search</button>
                 </section>
@@ -294,6 +457,7 @@ export class HomePage extends React.Component<IHomePageProps, IHomePageState> {
 
     onSearchClicked = () => {
         // Must verify filters are set in order to search
+        this.assembleFlightSearchList();
         this.getFlightOfferAPIData();
     }
 
