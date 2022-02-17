73f31d20a0608dbc003af2937de0f6624431b8b8
Dan McCarthy <dan.mccarthy.software@gmail.com>
2022-02-17 04:05:14 -0600

    Port Flight Reservation posting from Home Page to Checkout Page
    Moves the process of posting the reservations from the HomePage (on submit) to the checkout page for submission.

--- src/CheckoutPage.tsx
+++ src/CheckoutPage.tsx
@@ -5,6 +5,12 @@ import {ImageCarousel} from "./components/ImageCarousel";
 import './styles/CheckoutPage.css';
 import moment = require("moment");
 import { IFlightOfferData } from "./api/flightOffers";
+import { IToastMessage, ToastMessage } from "./components/ToastMessage";
+import { ToastType } from "./enums/ToastType";
+import { IFlightRequestInfo, IReservationData, registerReservations, TravelerType } from "./api/reservations";
+import { getAirlineNameFromIataCode } from "./lib/AirlineMapping";
+import { FlightType, flightTypeAsJsonLabel } from "./enums/FlightType";
+import { FlightClass, flightClassAsJsonLabel } from "./enums/FlightClass";
 
 interface ICheckoutPageState {
     bannerImages: string[];
@@ -12,18 +18,28 @@ interface ICheckoutPageState {
     lastName: string;
     birthday: moment.Moment;
     email: string;
+
+    // Error/Message Toast display and configuration
+    showToast: boolean;
+    toastMessage: IToastMessage;
 }
 
 interface ICheckoutPageProps{
     id_Token: string | null
     isLoggedIn: boolean
     reservedFlightOffers: IFlightOfferData[]
+    numAdultTravelers: number
+    numChildTravelers: number
+    flightType: FlightType
+    flightClass: FlightClass
 }
 
 export class CheckoutPage extends React.Component<ICheckoutPageProps, ICheckoutPageState> {
 
     public constructor(props: ICheckoutPageProps) {
         super(props)
+
+        console.log('From checkoutpage; flight offers:' + JSON.stringify(this.props.reservedFlightOffers));
 
         this.updateFirstName.bind(this);
         this.updateLastName.bind(this);
@@ -35,7 +51,11 @@ export class CheckoutPage extends React.Component<ICheckoutPageProps, ICheckoutP
             firstName: "",
             lastName: "",
             birthday: moment(),
-            email: ""
+            email: "",
+
+            // Initialize toast data, invisible by default until is configured for a message to be shown.
+            toastMessage: { toastType: ToastType.InfoToast, message: "" },
+            showToast: false,
         }
     }
 
@@ -99,7 +119,106 @@ export class CheckoutPage extends React.Component<ICheckoutPageProps, ICheckoutP
                 <div id='bannerCarousel'>
                     <ImageCarousel height={500} imagesToUse={this.state.bannerImages} />
                 </div>
+
+                <ToastMessage toastType={this.state.toastMessage.toastType} show={this.state.showToast} message={this.state.toastMessage.message} onToastClosed={this.onToastClosed}></ToastMessage>
             </div>
         )
+    }
+
+    onToastClosed = () => {
+        this.setState({
+            showToast: false
+        });
+    }
+
+    displayError = (message: string) => {
+        this.setState({
+            showToast: true,
+            toastMessage: {
+                message,
+                toastType: ToastType.ErrorToast
+            }
+        });
+    }
+
+    displaySuccess = (message: string) => {
+        this.setState({
+            showToast: true,
+            toastMessage: {
+                message,
+                toastType: ToastType.SuccessToast
+            }
+        });
+    }
+
+    submitReservations = async (): Promise<boolean> => {
+        const userId = this.props.id_Token;
+        if (userId == null) {
+            this.displayError('Failed to reserve flights due to not being logged in.');
+            return false;
+        }
+
+        // Will need to submit a copy of every flight once for each passenger
+        const numAdults = this.props.numAdultTravelers;
+        const numChildren = this.props.numChildTravelers;
+        const flightsToReserve = this.props.reservedFlightOffers;
+        const finalReservations: IReservationData[] = [];
+
+        flightsToReserve.forEach(async flight => {
+            // Submit reservations for each traveler
+            let adultsLeft = numAdults;
+            let childrenLeft = numChildren;
+
+            while (adultsLeft > 0 || childrenLeft > 0) {
+                // Figure out what kind of traveler we are reserving for and ensure they are counted as reserved.
+                const travelerType = adultsLeft > 0 ? TravelerType.Adult : TravelerType.Child;
+                if (travelerType === TravelerType.Adult) {
+                    adultsLeft -= 1;
+                } else {
+                    childrenLeft -= 1;
+                }
+
+                const itinerary = flight.itineraries[0];
+                const flightRequestInfo: IFlightRequestInfo[] = itinerary.segments.map(segment => {
+                    return {
+                        arrival_airport: segment.arrival.iataCode,
+                        departure_airport: segment.departure.iataCode,
+                        departure_time: segment.departure.at,
+                        arrival_time: segment.arrival.at,
+                        flight_provider: getAirlineNameFromIataCode(segment.carrierCode),
+                        flight_code: ''
+                    }
+                });
+
+                const reservation: IReservationData = {
+                    price: parseFloat(!!flight.price.grandTotal ? flight.price.grandTotal : '0'),
+                    trip_type: flightTypeAsJsonLabel(this.props.flightType),
+                    traveler_type: travelerType,
+                    traveler_name: 'FlightPassenger',
+                    seat_id: '',
+                    seat_class: flightClassAsJsonLabel(this.props.flightClass),
+                    num_checked_bags: 0,
+                    flight_request_info: flightRequestInfo
+                };
+
+                finalReservations.push(reservation);
+            }
+        });
+
+        const response: Response | Error = await registerReservations(finalReservations, userId);
+
+        if (response instanceof Error) {
+            this.displayError('Failed to send reservation submission to Dino Travel.');
+            return false;
+        }
+
+        if (response.status !== 200) {
+            this.displayError('Failed to register reservation.');
+            console.error(`Reservation registration failed with error status '${response.status} and error: '${response.statusText}'.'`);
+            return false;
+        } else {
+            this.displaySuccess(`Success! Your flight has now been booked. We'll now show you the flight details.`);
+            return true;
+        }
     }
 }
--- src/PageRouting.tsx
+++ src/PageRouting.tsx
@@ -7,6 +7,8 @@ import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
 import { SupportPage } from "./SupportPage";
 import { IFlightOfferData } from "./api/flightOffers";
+import { FlightClass } from "./enums/FlightClass";
+import { FlightType } from "./enums/FlightType";
 
 interface IPageRoutingProps {
 
@@ -18,25 +20,27 @@ interface IPageRoutingState {
     reservedFlights: IFlightOfferData[]
     reservedAdultSeats: number
     reservedChildSeats: number
+    flightClass: FlightClass
+    flightType: FlightType
 }
 
 export class PageRouting extends React.Component<IPageRoutingProps, IPageRoutingState> {
     constructor(props: IPageRoutingProps) {
         super(props)
-        this.state = {IDToken: null, isLoggedIn: false, reservedFlights: [], reservedAdultSeats: 0, reservedChildSeats: 0}
+        this.state = {IDToken: null, isLoggedIn: false, reservedFlights: [], reservedAdultSeats: 0, reservedChildSeats: 0, flightClass: FlightClass.BusinessClass, flightType: FlightType.MultiCity}
         let TokenJson = JSON.parse(localStorage.getItem('Token') as string)
         console.log(TokenJson)
         if (TokenJson != null) {
             let TimeExpired = TokenJson.expires_at
             if (Date.now() >= TimeExpired) {
-                this.state = {IDToken: null, isLoggedIn: false, reservedFlights: [], reservedAdultSeats: 0, reservedChildSeats: 0}
+                this.state = {IDToken: null, isLoggedIn: false, reservedFlights: [], reservedAdultSeats: 0, reservedChildSeats: 0, flightClass: FlightClass.BusinessClass, flightType: FlightType.MultiCity}
                 localStorage.setItem('LoggedIn', 'false');
                 localStorage.setItem('LoginAttempted', 'false');
                 localStorage.removeItem('Profile');
                 localStorage.removeItem('Token');
                 console.log('Token has expired');
             } else {
-                this.state = {IDToken: TokenJson.id_token, isLoggedIn: true, reservedFlights: [], reservedAdultSeats: 0, reservedChildSeats: 0};
+                this.state = {IDToken: TokenJson.id_token, isLoggedIn: true, reservedFlights: [], reservedAdultSeats: 0, reservedChildSeats: 0, flightClass: FlightClass.BusinessClass, flightType: FlightType.MultiCity};
             }
         }
     }
@@ -50,11 +54,13 @@ export class PageRouting extends React.Component<IPageRoutingProps, IPageRouting
         this.render();
     }
 
-    updateReservedFlights = (reservedFlights: IFlightOfferData[], reservedAdultSeats: number, reservedChildSeats: number) => {
+    updateReservedFlights = (reservedFlights: IFlightOfferData[], reservedAdultSeats: number, reservedChildSeats: number, flightClass: FlightClass, flightType: FlightType) => {
         this.setState({
             reservedFlights,
             reservedAdultSeats,
-            reservedChildSeats
+            reservedChildSeats,
+            flightClass,
+            flightType
         });
     }
 
@@ -64,7 +70,7 @@ export class PageRouting extends React.Component<IPageRoutingProps, IPageRouting
                 <Routes>
                     <Route path="/support" element={<SupportPage/>} />
                     <Route path="/about" element={<AboutPage />} />
-                    <Route path="/checkout" element={<CheckoutPage id_Token={this.state.IDToken} isLoggedIn={this.state.isLoggedIn} reservedFlightOffers={this.state.reservedFlights} />} />
+                    <Route path="/checkout" element={<CheckoutPage id_Token={this.state.IDToken} isLoggedIn={this.state.isLoggedIn} reservedFlightOffers={this.state.reservedFlights} numAdultTravelers={this.state.reservedAdultSeats} numChildTravelers={this.state.reservedChildSeats} flightClass={this.state.flightClass} flightType={this.state.flightType} />} />
                     <Route path="/" element={<HomePage id_Token={this.state.IDToken} isLoggedIn={this.state.isLoggedIn} onReservedFlightsFinalized={this.updateReservedFlights} />} />
                     <Route path="/login" element={<LoginPage updateIDToken={this.updateIdToken} isLoggedIn={this.state.isLoggedIn}/>} />
                 </Routes>
